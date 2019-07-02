e346f76ee9b293d617fbfc7850c5cfe7245f0b39
ZoraGK <jyh199759@126.com>
2019-07-02 16:24:11 +0800

    临时测试

---
+++ src/com/zora/AplusB.java
@@ -0,0 +1,15 @@
+package com.zora;
+
+import java.util.Scanner;
+
+/**
+ * 临时测试用的，我连索引都没写，就是不想被看到，略略略
+ */
+public class AplusB {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int A=scanner.nextInt();
+        int B=scanner.nextInt();
+        System.out.print(A+B);
+    }
+}
