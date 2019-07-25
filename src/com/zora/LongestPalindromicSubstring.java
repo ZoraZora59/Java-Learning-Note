73b9cdb25d672039801796048ccd7f70272f42b6
ZoraGK <jyh199759@126.com>
2019-07-25 11:10:56 +0800

    RSA加密

--- src/com/zora/LongestPalindromicSubstring.java
+++ src/com/zora/LongestPalindromicSubstring.java
@@ -12,31 +12,6 @@ public class LongestPalindromicSubstring {
         System.out.println("Output : " + string);
     }
     private String longestPalindrome(String s) {
-        if (s == null || " ".equals(s) || "".equals(s)) {
-            return "";
-        } else if (s.length() < 2) {
-            return s;
-        }
-        HashMap<Character, Integer> map=new HashMap<>(1000);
-        char[] str = s.toCharArray();
-        int p1=0,p2=str.length-1;
-        for (int i=p1;i<=p2;i++) {
-            if(!map.containsKey(str[i])){
-                map.put(str[i],i);
-            }
-        }
-        int max=0;
-        char ch=str[0];
-        for(int i=p2;i>=p1;i--){
-            if((i-map.get(str[i]))>max){
-                ch=str[i];
-                max=i-map.get(str[i]);
-            }
-        }
-        String ret="";
-        for(int i=map.get(ch),j=0;j<=max;i++,j++){
-            ret=ret+str[i];
-        }
-        return ret;
+        return null;
     }
 }
