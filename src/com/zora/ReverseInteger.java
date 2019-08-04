bb506c35fdf718132023e5af6bbc66818a27ed5e
ZoraGK <jyh199759@126.com>
2019-08-05 00:04:24 +0800

    数字倒置

---
+++ src/com/zora/ReverseInteger.java
@@ -0,0 +1,27 @@
+package com.zora;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int input = -2147483412;
+        System.out.println(new ReverseInteger().reverse(input));
+    }
+
+    public int reverse(int x) {
+        if (x > -10 && x < 10) {
+            return x;
+        }
+        int ret = 0;
+        while (x != 0) {
+            int rest = x % 10;
+            x /= 10;
+            if (ret > Integer.MAX_VALUE / 10 || (ret == Integer.MAX_VALUE / 10 && rest > 7)) {
+                return 0;
+            }
+            if (ret < Integer.MIN_VALUE / 10 || (ret == Integer.MIN_VALUE / 10 && rest < -8)) {
+                return 0;
+            }
+            ret = ret * 10 + rest;
+        }
+        return ret;
+    }
+}
