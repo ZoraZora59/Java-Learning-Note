fc00231491ffe61ced7cfc0de199e7d4d2acff0a
纪玉涵 <jiyh@newgrand.cn>
2019-08-30 16:34:06 +0800

    添加处理机制

--- src/com/zora/DES.java
+++ src/com/zora/DES.java
@@ -2,6 +2,7 @@ package com.zora;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -18,34 +19,41 @@ import javax.crypto.spec.SecretKeySpec;
  **/
 public class DES {
     public static void main(String[] args) {
-
-        String msg =
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                        "<Transaction>\n" +
-                        "    <Transaction_Header>\n" +
-                        "        <SYS_TX_CODE><![CDATA[P1OPME001]]></SYS_TX_CODE>\n" +
-                        "        <SYS_MSG_LEN><![CDATA[0000000777]]></SYS_MSG_LEN>\n" +
-                        "        <SYS_REQ_TIME><![CDATA[20190827133334399]]></SYS_REQ_TIME>\n" +
-                        "        <SYS_TX_VRSN><![CDATA[01]]></SYS_TX_VRSN>\n" +
-                        "        <TXN_DT><![CDATA[20190827]]></TXN_DT>\n" +
-                        "        <TXN_TM><![CDATA[133334]]></TXN_TM>\n" +
-                        "        <TXN_STFF_ID><![CDATA[555555]]></TXN_STFF_ID>\n" +
-                        "        <MULTI_TENANCY_ID><![CDATA[CN000]]></MULTI_TENANCY_ID>\n" +
-                        "        <LNG_ID><![CDATA[zh-cn]]></LNG_ID>\n" +
-                        "        <REC_IN_PAGE></REC_IN_PAGE>\n" +
-                        "        <PAGE_JUMP></PAGE_JUMP>\n" +
-                        "        <STS_TRACE_ID></STS_TRACE_ID>\n" +
-                        "        <CHNL_CUST_NO><![CDATA[JS32000009020921001]]></CHNL_CUST_NO>\n" +
-                        "        <IttParty_Jrnl_No></IttParty_Jrnl_No>\n" +
-                        "        <Txn_Itt_IP_Adr><![CDATA[128.3.81.208]]></Txn_Itt_IP_Adr>\n" +
-                        "    </Transaction_Header>\n" +
-                        "    <Transaction_Body></Transaction_Body>\n" +
-                        "</Transaction>";;
-        String password = "zZ6U4IzV/kq2fHYTXkwNL82elOCM1f5K";
-        String s1 = DES3.DESedeEncrypt(msg, password);
-        System.out.println("加密:" + s1);
-        System.out.println("解密:" + DES3.DESedeDecrypt(s1, password));
+        try {
+            String msg =
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                            "<Transaction>\n" +
+                            "    <Transaction_Header>\n" +
+                            "        <SYS_TX_CODE><![CDATA[P1OPME001]]></SYS_TX_CODE>\n" +
+                            "        <SYS_MSG_LEN><![CDATA[0000000777]]></SYS_MSG_LEN>\n" +
+                            "        <SYS_REQ_TIME><![CDATA[20190827133334399]]></SYS_REQ_TIME>\n" +
+                            "        <SYS_TX_VRSN><![CDATA[01]]></SYS_TX_VRSN>\n" +
+                            "        <TXN_DT><![CDATA[20190827]]></TXN_DT>\n" +
+                            "        <TXN_TM><![CDATA[133334]]></TXN_TM>\n" +
+                            "        <TXN_STFF_ID><![CDATA[555555]]></TXN_STFF_ID>\n" +
+                            "        <MULTI_TENANCY_ID><![CDATA[CN000]]></MULTI_TENANCY_ID>\n" +
+                            "        <LNG_ID><![CDATA[zh-cn]]></LNG_ID>\n" +
+                            "        <REC_IN_PAGE></REC_IN_PAGE>\n" +
+                            "        <PAGE_JUMP></PAGE_JUMP>\n" +
+                            "        <STS_TRACE_ID></STS_TRACE_ID>\n" +
+                            "        <CHNL_CUST_NO><![CDATA[JS32000009020921001]]></CHNL_CUST_NO>\n" +
+                            "        <IttParty_Jrnl_No></IttParty_Jrnl_No>\n" +
+                            "        <Txn_Itt_IP_Adr><![CDATA[128.3.81.208]]></Txn_Itt_IP_Adr>\n" +
+                            "    </Transaction_Header>\n" +
+                            "    <Transaction_Body></Transaction_Body>\n" +
+                            "</Transaction>";
+            ;
+            String password = "zZ6U4IzV/kq2fHYTXkwNL82elOCM1f5K";
+            String s1 = DES3.DESedeEncrypt(msg, password);
+            System.out.println("加密:" + s1);
+            //s1 = DES3.DESedeDecrypt(s1, password);
+            byte[] bt = s1.getBytes();
+            System.out.println("解密:" + Arrays.toString(bt));
+        } catch (Exception ex) {
+            System.out.println("Error");
+        }
     }
+
 }
 
 class DES3 {
@@ -53,7 +61,7 @@ class DES3 {
     private static final String KEY_ALGORITHM = "DESede";
     /**
      * 默认的加密算法
-      */
+     */
     private static final String DEFAULT_CIPHER_ALGORITHM = "DESede/ECB/PKCS5Padding";
 
     /**
