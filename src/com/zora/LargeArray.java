f3d01c989ba34697befb29b2db7ad3eb406f650c
ZoraZora <zora759gk@gmail.com>
2019-11-16 12:36:36 +0800

    保障编译成功，暂时注释了两个文件，类型转换对比需要糊涂工具箱的包

--- src/com/zora/LargeArray.java
+++ src/com/zora/LargeArray.java
@@ -1,66 +1,66 @@
-package com.zora;
-
-import com.sun.tools.javac.code.Attribute;
-
-import java.lang.reflect.Array;
-
-/**
- * <h3>Java-Learning-Note</h3>
- * <p>大数组快速排序</p>
- *
- * @author : zora
- * @date : 2019/10/16
- */
-public class LargeArray {
-    public static void main(String[] args) {
-        int[] largeIntArray = new int[1000000];
-
-    }
-}
-
-class Solution {
-        int thirdMax (vector<int> & nums) {
-        int nSize = nums.size();
-        if (0 == nSize) {
-            return -1;
-        } else if (1 == nSize) {
-            return nums[0];
-        } else if (2 == nSize) {
-            return nums[0] > nums[1] ? nums[0] : nums[1];
-        }
-
-        int nMax = -2147483648;
-        int nSec = -2147483648;
-        int nThird = -2147483648;
-        bool bRes = false;
-        int count = nSize;
-
-        for (int i = 0; i < nSize; i++) {
-            if (nums[i] == -2147483648 && !bRes) {
-                bRes = true;
-                continue;
-            }
-            if (nMax == nums[i] || nSec == nums[i] || nThird == nums[i]) {
-                count--;
-            }
-
-            if (nMax < nums[i]) {
-                nThird = nSec;
-                nSec = nMax;
-                nMax = nums[i];
-            } else if (nSec < nums[i] && nMax != nums[i]) {
-                nThird = nSec;
-                nSec = nums[i];
-            } else if (nThird < nums[i] && nSec != nums[i] && nMax != nums[i]) {
-                nThird = nums[i];
-            }
-        }
-
-
-        if (count >= 3) {
-            return nThird;
-        }
-        return nMax;
-    }
-    }
-}
+//package com.zora;
+//
+//import com.sun.tools.javac.code.Attribute;
+//
+//import java.lang.reflect.Array;
+//
+///**
+// * <h3>Java-Learning-Note</h3>
+// * <p>大数组快速排序</p>
+// *
+// * @author : zora
+// * @date : 2019/10/16
+// */
+//public class LargeArray {
+//    public static void main(String[] args) {
+//        int[] largeIntArray = new int[1000000];
+//
+//    }
+//}
+//
+//class Solution {
+//        int thirdMax (vector<int> & nums) {
+//        int nSize = nums.size();
+//        if (0 == nSize) {
+//            return -1;
+//        } else if (1 == nSize) {
+//            return nums[0];
+//        } else if (2 == nSize) {
+//            return nums[0] > nums[1] ? nums[0] : nums[1];
+//        }
+//
+//        int nMax = -2147483648;
+//        int nSec = -2147483648;
+//        int nThird = -2147483648;
+//        bool bRes = false;
+//        int count = nSize;
+//
+//        for (int i = 0; i < nSize; i++) {
+//            if (nums[i] == -2147483648 && !bRes) {
+//                bRes = true;
+//                continue;
+//            }
+//            if (nMax == nums[i] || nSec == nums[i] || nThird == nums[i]) {
+//                count--;
+//            }
+//
+//            if (nMax < nums[i]) {
+//                nThird = nSec;
+//                nSec = nMax;
+//                nMax = nums[i];
+//            } else if (nSec < nums[i] && nMax != nums[i]) {
+//                nThird = nSec;
+//                nSec = nums[i];
+//            } else if (nThird < nums[i] && nSec != nums[i] && nMax != nums[i]) {
+//                nThird = nums[i];
+//            }
+//        }
+//
+//
+//        if (count >= 3) {
+//            return nThird;
+//        }
+//        return nMax;
+//    }
+//    }
+//}
